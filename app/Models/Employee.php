114aa67c0b71d19061fc8043c37acd74314f4fbf
RanaaAhuniess <rana.hanis93@gmail.com>
2023-05-26 22:24:29 +0300

    Added Employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $dates = ['dob', 'join_date', 'created_at', 'updated_at'];
+    protected $fillable = ['user_id', 'first_name', 'last_name', 'sex', 'dob', 'join_date', 'salary'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_05_26_141357_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->dateTime('dob');
+            $table->string('gender');
+            $table->dateTime('join_date');
+            $table->float('salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
