8e2c71099d32e4a21ad7fcf4fc92bdf711dea2ef
Rana <rana.hanis93@gmail.com>
2023-05-26 17:05:28 +0300

    Added passport package

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,18 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+//        Passport::routes();
+        // Mandatory to define Scope
+        Passport::tokensCan([
+            'super-admin' => 'Can create all types of users, edit all user\'s info, and delete all types of users',
+            'hr' => 'Can approve or decline leave requests, change leave balance for all employees, and manage leave types',
+            'employee' => 'Can request leave with specific type and view leave history',
+        ]);
+
+        Passport::setDefaultScope([
+            'employee'
+        ]);
+
     }
 }
