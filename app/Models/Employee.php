0b70c894898ff03a51678042c2a6e32188e39722
RanaaAhuniess <rana.hanis93@gmail.com>
2023-05-27 22:32:26 +0300

    Added employee show and delete functionalities

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $dates = ['dob', 'join_date', 'created_at', 'updated_at'];
     protected $fillable = ['user_id', 'first_name', 'last_name', 'gender', 'dob', 'join_date', 'salary'];
